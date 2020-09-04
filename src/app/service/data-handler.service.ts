875dacd9ff1291354086ac64d40defdcf6f38f27
serghalak <1802743Serg>
2020-09-04 20:58:46 +0300

    add showing all tasks (without filtering by category)

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Category} from '../model/Category';
 import {TestData} from '../data/TestData';
+import {Task} from '../model/Task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() { }
+  constructor() {
+  }
 
-  getCategories(): Category[]{
+  getCategories(): Category[] {
     return TestData.categories;
+  }
+
+  getTasks(): Task[] {
+    return TestData.tasks;
   }
 }
